70bc945dede88f77933101dd8b805908418c8004
jithin varghese <jithin6manjapra@gmail.com>
2021-07-24 12:44:09 +1200

    constructor

---
+++ const/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int a=10;
+		
+		sample s1=new sample(a);
+		s1.display();
+
+	}
+
+}
---
+++ const/src/sample.java
@@ -0,0 +1,16 @@
+
+public class sample {
+	int z;
+	
+	sample(int a){
+		this.z=a;
+		System.out.println("heyy");
+		display();
+		
+	}
+
+	
+	void display() {
+		System.out.println("abc"+z);
+	}
+}
