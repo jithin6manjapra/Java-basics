f0de9e63fb63ee6ac3eb3bd4fcebdc4d1cb45adb
jithin varghese <jithin6manjapra@gmail.com>
2021-07-24 13:07:13 +1200

    inheritance

---
+++ inheritance/src/inheritance/main.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+public class main {
+	main(){
+		System.out.println("first");
+	}
+	
+	int k=1;
+	public static void main(String[] args) {
+		
+	sample k=new sample();
+	k.display();
+	
+
+	}
+
+}
---
+++ inheritance/src/inheritance/sample.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class sample extends main{
+	sample(){
+		System.out.println("second");
+	}
+	
+	int num=10;
+	void display() {
+		System.out.println("abc"+num+k);
+	}
+
+}
